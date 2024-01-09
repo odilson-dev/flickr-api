717253b7447629772c90e8c525f3755c19297e2f
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-01-09 09:23:39 -0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
+  root to: 'static_pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
